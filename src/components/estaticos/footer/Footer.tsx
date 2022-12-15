f515d570a6e0b725e1c63df59c28ecd1939c03f7
David Balzarini <davidpereira2302@gmail.com>
2022-12-15 14:40:52 -0300

    deploy

---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,55 @@
+import { Grid, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import React from 'react'
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import './Footer.css'
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducers';
+
+function Footer(){
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+    
+    var footerComponent;
+    if(token != ''){
+        footerComponent = <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid alignItems="center" item xs={12}>
+            <Box className='box1'>
+                <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                    <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                </Box>
+                <Box display="flex" alignItems="center" justifyContent="center">
+                    <a href="https://www.facebook.com/davidbalzarini" target="_blank">
+                        <FacebookIcon className='redes' />
+                    </a>
+                    <a href="https://www.instagram.com/davidbalzarini" target="_blank">
+                        <InstagramIcon className='redes' />
+                    </a>
+                    <a href="https://www.linkedin.com/in/david-balzarini-pereira-645481227/" target="_blank">
+                        <LinkedInIcon className='redes' />
+                    </a>
+                    <a href="https://github.com/davidbalzarini" target="_blank">
+                        <GitHubIcon className='redes2' />
+                    </a>
+                </Box>
+            </Box>
+            <Box className='box2'>
+                <Box paddingTop={1}>
+                    <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2022 Copyright:</Typography>
+                </Box>
+            </Box>
+        </Grid>
+    </Grid>
+    }
+    return(
+        <>
+        {footerComponent}
+        </>
+    );
+}
+
+export default Footer;
